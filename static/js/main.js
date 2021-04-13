2bc9ae9ff9053b3ee0315ce6ba137a70f98e8319
csingh874 <58983507+csingh874@users.noreply.github.com>
2021-04-13 19:27:16 +0530

    changed main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,44 +1,41 @@
-document.getElementById('home_form').addEventListener("submit", function(e){
-    e.preventDefault();
-    document.getElementById("btn_modal").click();
-});
-document.getElementById("modal_form").addEventListener("submit", modal_field_validation);
+document.getElementById('btn_proceed').addEventListener("click", field_validation);
+
+btn_submit = document.getElementById("submit_data");
+if (btn_submit!= undefined){
+    btn_submit.addEventListener("click", modal_field_validation);
+}
+
 document.getElementById("btn_close").addEventListener("click",add_impression);
 
-// Function to get Authentication Token
-function get_token(){
+function add_impression() {
+    quote_token = tg.get('token');
     fetch("https://connect-sandbox.ticketguardian.net/api/v2/auth/token/",
     {
         method: "POST",
         headers:{
-            'Content-type': 'application/json'
+        'Content-type': 'application/json'
         },
         body: JSON.stringify({"public_key": "pk_sandbox_c24dc55e4d07719b80c0916ce8a28e4dbf6a048f",
                 "secret_key": "sk_sandbox_ea7865b84b0f4b762bd2d934ad1f750b84b5a3ba"})
     })
     .then(response => {return response.json()})
-    .then((data) => {return data.token});
-}
-
-function add_impression() {
-    quote_token = tg.get('token');
-    console.log(get_token())
-    get_token().then((data) => { fetch("https://api.ticketguardian-sandbox.com/impressions/",
+    .then((data) => { fetch("https://api.ticketguardian-sandbox.com/impressions/",
         {
             method:"POST",
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': "JWT "+  data
+                'Authorization': "JWT "+  data.token
             },
             body: JSON.stringify({"quote_token": quote_token})
         })
         .then(response => {return response.json()})
-        .then(data1 => console.log(data1));
+        .then(data1 => console.log(data1))
         }
     );
 }
 
 function retrieve_data() {
+    console.log("retrieve DAta");
     // getting customer Data
     var customer_details = document.getElementById("customer").getElementsByClassName("form-control");
     let customer_data = extract_data(customer_details);
@@ -65,7 +62,7 @@ function retrieve_data() {
     let card_details = extract_data(card_data);
     // Retrieving Email Address and storing it in customer_data instance
     customer_data["email"] = document.getElementById("email").value;
-    // consolidating the required data
+    // consolading the required data
     let all_data = {
         "quote": tg.get("token"),
         "customer": customer_data,
@@ -80,34 +77,44 @@ function retrieve_data() {
 }
 
 function send_request(all_data){
-    get_token().then((data) => {fetch("https://connect-sandbox.ticketguardian.net/api/v2/orders/",
+   fetch("https://connect-sandbox.ticketguardian.net/api/v2/auth/token/",
     {
         method: "POST",
-        headers: {'Content-type': 'application/json',
-                  'Accept': 'application/json',
-                  'Authorization': "JWT " + data.token,
-                  },
-        body: JSON.stringify(all_data)
+        headers:{
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({"public_key": "pk_sandbox_c24dc55e4d07719b80c0916ce8a28e4dbf6a048f",
+                "secret_key": "sk_sandbox_ea7865b84b0f4b762bd2d934ad1f750b84b5a3ba"})
     })
-    .then(response => {
-             return response.json()})
-    .then(data1 => {
-        if(data1.error){
-            var errors_list = new Array();
-            for (var x in data1.error.errors){
-                errors_list.push(data1.error.errors[x].message);
-                }
-                document.getElementById("display-error").innerHTML =
-                 `<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                       ${errors_list}
-                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
+    .then(response => {return response.json()})
+    .then((data) => {fetch("https://connect-sandbox.ticketguardian.net/api/v2/orders/",
+                    {
+                        method: "POST",
+                        headers: {'Content-type': 'application/json',
+                                  'Accept': 'application/json',
+                                  'Authorization': "JWT " + data.token,
+                                  },
+                        body: JSON.stringify(all_data)
+                    })
+                    .then(response => {
+                             return response.json()})
+                    .then(data1 => {
+                        if(data1.error){
+                            var errors_list = new Array();
+                            for (var x in data1.error.errors){
+                                errors_list.push(data1.error.errors[x].message);
+                                }
+                                document.getElementById("display-error").innerHTML =
+                                 `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                       ${errors_list}
+                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
+                            }
+                        else{
+                               window.location.href = "success/"+data1.order_number
+                        }
+                        });
+                    });
             }
-        else{
-               window.location.href = "success/"+data1.order_number
-        }
-        });
-    });
-}
 
 function extract_data(data){
     let new_data = {};
@@ -117,46 +124,66 @@ function extract_data(data){
     return new_data;
 }
 
-//Validation Method for Modal Inputs
-function modal_field_validation(e){
-    e.preventDefault();
-    date = new Date();
-    year = date.getFullYear();
-    year = parseInt(year.toString().slice(2,));
-    mnth = date.getMonth() + 1;
-    exp_yr = document.getElementById("expiry_year")
-    exp_mnth = document.getElementById("expiry_month");
-    if (parseInt(exp_yr.value) == year){
-        if ((parseInt(exp_mnth.value)== mnth) || (parseInt(exp_mnth.value) < mnth)){
-            console.log("Year and month Error");
-            setErrorFor(exp_yr, "Your card has been expired.");
-        }
+function field_validation(){
+     const required_fields = ["order_number", "first_name", "last_name", "address1", "city", "state", "zip_code", "country","name", "reference_number", "cost"];
+     const messages = ["Order Number", "First Name", "Last Name", "Address 1", "City", "State", "Zip Code", "Country", "Name", "Reference Number", "Cost"];
+     let error = false;
+    // Validating Field
+    for(let x in required_fields){
+            field = document.getElementById(required_fields[x]);
+            if (field.value === ""){
+                setErrorFor(field, messages[x] +" is required field");
+                error = true;
+
+            }
+            else{
+                setSuccessFor(field);
+            }
     }
-    else if(parseInt(exp_yr.value) < year){
-        setErrorFor(exp_yr, "Your card has been expired.");
+
+    if (error == false){
+        document.getElementById("btn_modal").click();
+
     }
-    else{
-        retrieve_data();
-        console.log("success");
-    }
+
 }
 
-// If there is a Validation error it will change the color of input field and small tag to red
+function modal_field_validation(){
+     const required_fields = ["email", "number", "expiry_month", "expiry_year", "cvv"];
+     const messages = ["Email", "Card Number", "Expiry Month", "Expiry Year", "CVV"];
+     let error = false;
+    // Validating Field
+    for(let x in required_fields){
+            field = document.getElementById(required_fields[x]);
+            if (field.value === ""){
+                setErrorFor(field, messages[x] +" is required field");
+                error = true;
+            }
+            else{
+                setSuccessFor(field);
+            }
+    }
+
+    if (error == false){
+        retrieve_data();
+    }
+
+}
+
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
 	const label = formControl.querySelector('label');
-	label.className = 'form-label text-danger';
+	label.className = 'bmd-label-floating text-danger';
 	const small = formControl.querySelector('small');
 	input.className = 'form-control error';
 	small.innerText = message;
 	small.className = "text-danger";
 }
 
-// if there is no validation error then it will keep input as normal
 function setSuccessFor(input) {
     const formControl = input.parentElement;
 	const label = formControl.querySelector('label');
-	label.className = 'form-label';
+	label.className = 'bmd-label-floating';
 	const small = formControl.querySelector('small');
 	input.className = 'form-control';
 	small.innerText = "";
